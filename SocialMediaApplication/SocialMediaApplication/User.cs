74efb2f53b3d8d18fdf147e71ffa33598844cfc1
sk185730 <sk185730@ncr.com>
2022-06-07 15:17:15 +0530

    Committing the liskov substitution code

---
+++ SocialMediaApplication/SocialMediaApplication/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApplication
+{
+    class User
+    {
+        private int _id;
+
+        public int Id { get; set; }
+    }
+}
---
+++ SolidPrinciples/LiskovSubstitution/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LiskovSubstitution
+{
+    public interface IRead
+    {
+        public void Read();
+    }
+    public interface IWrite
+    {
+        public void Write();
+    }
+
+    //class User
+    //{
+    //    public virtual void Read()
+    //    {
+    //        Console.WriteLine("File read");
+    //    }
+    //    public virtual void Write()
+    //    {
+    //        Console.WriteLine("File written");
+    //    }
+    //}
+
+    class Admin : IRead, IWrite
+    {
+        public void Read()
+        {
+            Console.WriteLine("File read by admin");
+        }
+        public void Write()
+        {
+            Console.WriteLine("File written by admin");
+        }
+    }
+
+    class RegularUser : IRead
+    {
+        public void Read()
+        {
+            Console.WriteLine("File read by regular user");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRead iread = new RegularUser();
+            iread.Read();
+            iread = new Admin();
+            iread.Read();
+
+            IWrite user = new Admin();
+            user.Write();
+
+
+        }
+    }
+}
